3f3c661022cc26919c22057d97640dab3542f7bc
AavishChandraprakash.Singh <aavishchandraprakash.singh@cbre.com>
2020-12-08 13:50:58 +0530

    Updates

---
+++ errors/request-validation-error.ts
@@ -0,0 +1,14 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+  constructor(public errors: ValidationError[]) {
+    super('Failed schema validation');
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(error => ({ message: error.msg, field: error.param }));
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-interface Fruit {
-  name: string;
-  color: string;
-}
+// Errors
+export * from '../errors/bad-request-error';
+export * from '../errors/custom-error';
+export * from '../errors/database-connection-error';
+export * from '../errors/not-authorized-error';
+export * from '../errors/not-found-error';
+export * from '../errors/request-validation-error';
 
-const banana: Fruit = {
-  name: 'banana',
-  color: 'yellow'
-};
-
-console.log(banana);
+// Middlewares
+export * from '../middlewares/current-user';
+export * from '../middlewares/error-handler';
+export * from '../middlewares/require-auth';
+export * from '../middlewares/validate-request';
